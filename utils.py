43a219a2f6a43b4ca166eac47a377d0b3c2074d7
Hoang Chung Hien <hoangchunghien@gmail.com>
2018-03-02 11:42:12 +0700

    feat: Set 123456789 for empty boxes

--- utils.py
+++ utils.py
@@ -28,14 +28,14 @@ def display(values):
     return
 
 def grid_values(grid):
-    """Convert grid string into {<box>: <value>} dict with '.' value for empties.
+    """Convert grid string into {<box>: <value>} dict with '123456789' value for empties.
 
     Args:
         grid: Sudoku grid in string form, 81 characters long
     Returns:
         Sudoku grid in dictionary form:
         - keys: Box labels, e.g. 'A1'
-        - values: Value in corresponding box, e.g. '8', or '.' if it is empty.
+        - values: Value in corresponding box, e.g. '8', or '123456789' if it is empty.
     """
     assert len(grid) == 81, "Input grid must be a string of length 81 (9x9)"
-    return dict(zip(boxes, grid))
+    return dict((k, v if v != '.' else '123456789') for k, v in zip(boxes, grid))
