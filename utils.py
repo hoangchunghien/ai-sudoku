52080a25f2e5a9892538f508b73f5f3530b5f34a
Hoang Chung Hien <hoangchunghien@gmail.com>
2018-03-02 15:18:09 +0700

    feat: Add only_choice strategy

--- utils.py
+++ utils.py
@@ -59,3 +59,19 @@ def eliminate(values):
         for peer in peers[k]:
             values[peer] = values[peer].replace(digit, '')
     return values
+
+def only_choice(values):
+    """Finalize all values that are the only choice for a unit.
+
+    Go through all the units, and whenever there is a unit with a value
+    that only fits in one box, assign the value to this box.
+
+    Input: Sudoku in dictionary form.
+    Output: Resulting Sudoku in dictionary form after filling in only choices.
+    """
+    for unit in unitlist:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                values[dplaces[0]] = digit
+    return values
