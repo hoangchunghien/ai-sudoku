82903c526637a03667c7c3334e43d57f8de09075
Hoang Chung Hien <hoangchunghien@gmail.com>
2018-03-03 12:57:17 +0700

    feat: Add diagonal units

--- utils.py
+++ utils.py
@@ -9,7 +9,12 @@ boxes = cross(rows, cols)
 row_units = [cross(r, cols) for r in rows]
 col_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')]
+
 unitlist = row_units + col_units + square_units
+
+diagonal_units = [[x+y for x, y in zip(rows, cols)], [x+y for x, y in zip(rows[::-1], cols)]]
+unitlist = unitlist + diagonal_units
+
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
 
